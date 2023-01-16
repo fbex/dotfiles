88ddb3f684356bad388721ad83146593bca7cb13
fbex <fbex@posteo.de>
2023-01-16 21:14:24 +0100

    Add new neovim config

---
+++ .config/nvim/lua/fbex/packer.lua
@@ -0,0 +1,35 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use {
+		'morhetz/gruvbox',
+		as = 'gruvbox',
+		config = function()
+			vim.cmd("colorscheme gruvbox")
+		end
+	}
+
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
+
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+
+end)
